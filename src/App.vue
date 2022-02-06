ff1cadd283e5f16cf3fa1b63f76b7d7a1d1588ab
dmnitprof <dmn.itprof@gmail.com>
2022-02-06 23:50:42 +0300

    create vue 3  form

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,103 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container">
+    <h1>Auth</h1>
+    <form @submit.prevent="submitHandler" class="mb-3">
+      <h3 v-if="error">{{ error }}</h3>
+
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          v-model="form.email.value"
+          :class="{invalid: !form.email.valid  && form.email.touched}"
+          @blur="form.email.blur"
+        >
+        <small class="text-danger" v-if="form.email.touched && form.email.errors.required">Email field is
+          required</small>
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="password"
+          v-model="form.password.value"
+          :class="{invalid: !form.password.valid && form.password.touched}"
+          @blur="form.password.blur"
+        >
+        <small class="text-danger" v-if="form.password.touched && form.password.errors.required">Password field is
+          required</small>
+        <small class="text-danger" v-else-if="form.password.touched && form.password.errors.minLength">
+          Password length can't be less then <span class="text-primary">8</span>. Now it is
+          <span class="text-primary">{{ form.password.value.length }}</span>.
+        </small>
+      </div>
+      <button type="submit" class="btn btn-primary" :disabled="!form.valid">Submit</button>
+    </form>
+    <Suspense v-if="submitted">
+      <UserList/>
+      <template #fallback>
+        <div class="text-center">
+          <div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      </template>
+    </Suspense>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { useForm } from '@/use/form'
+import UserList from '@/components/UserList'
+import { ref, onErrorCaptured } from 'vue'
 
-#nav {
-  padding: 30px;
+const required = val => !!val
+const minLength = num => val => val.length >= num
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+export default {
+  components: { UserList },
+  setup () {
+    const submitted = ref(false)
+    const error = ref()
+    const form = useForm({
+      email: {
+        value: '',
+        validators: { required }
+      },
+      password: {
+        value: '',
+        validators: {
+          required,
+          minLength: minLength(8)
+        }
+      }
+    })
 
-    &.router-link-exact-active {
-      color: #42b983;
+    function submitHandler () {
+      console.log('Email', form.email.value)
+      console.log('Password', form.password.value)
+      submitted.value = true
+    }
+
+    onErrorCaptured(e => {
+      error.value = e.message
+    })
+
+    return {
+      form,
+      submitHandler,
+      submitted,
+      error
     }
   }
+}
+</script>
+
+<style>
+.invalid {
+  box-shadow: 0 2px 4px brown, 0 2px 8px brown;
 }
 </style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <ul class="list">
+      <li class="list-item" v-for="user in users" :key="user.id">
+        {{user}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { useUsers } from '@/use/users'
+
+export default {
+  name: 'UserList',
+  async setup () {
+    const { users } = await useUsers()
+    return {
+      users
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
